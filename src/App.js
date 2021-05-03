9e9f05ea02048a04570b3f488b9f5879c995f1d1
Sidharth <sidnagar08@gmail.com>
2021-05-03 13:56:50 +0530

    Added Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Counter from "./components/Counter"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    return(
+      <Counter />
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Counter.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+class Counter extends React.Component{
+    render(){
+        return <div>Test</div>
+    }
+}
+
+export default Counter;
