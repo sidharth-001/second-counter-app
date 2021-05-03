f37e4a52143652b4d208eff1a8a5e2ad7a1950e3
Sidharth <sidnagar08@gmail.com>
2021-05-03 15:14:52 +0530

    counting...

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,8 +1,27 @@
 import React from "react"
 
 class Counter extends React.Component{
+
+    constructor(){
+        super();
+        this.state = {
+            counter: 0
+        };
+        this.timer = null;
+    }
+
+    componentDidMount(){
+        this.timer = setInterval(() => {
+            this.setState({counter: this.state.counter+1});
+        }, 1000);
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timer);
+    }
+
     render(){
-        return <div>Test</div>
+        return <span>{this.state.counter}</span>
     }
 }
 
